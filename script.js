b549fd12a671cce19d8e15c0aac7a51dbff56374
Yizhuo-Liang <yizhuo.liang.hello@gmail.com>
2021-09-28 23:06:31 -0400

    applying boxes model

---
+++ script.js
@@ -0,0 +1,21 @@
+
+/*
+    Acknowledgment: code for make navbar sticky
+    modified from the tutorial of W3School
+*/
+
+window.onscroll = function() {makeSticky()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function makeSticky() {
+    if(navbar == null) console.info("okay"); 
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    console.info("????");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
